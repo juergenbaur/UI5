80f40a7018451b018cb58c82c238517abe16f3aa
Juergen Baur <juergen.baur@avisco.de>
2023-08-04 12:00:55 +0000

    json model

--- orderapp/webapp/controller/Main.controller.js
+++ orderapp/webapp/controller/Main.controller.js
@@ -1,24 +1,59 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller"
+    "sap/ui/core/mvc/Controller",
+    "sap/ui/model/json/JSONModel"
 ],
     /**
      * @param {typeof sap.ui.core.mvc.Controller} Controller
      */
-    function (Controller) {
+    function (Controller, JSONModel) {
         "use strict";
 
         return Controller.extend("zju.orderapp.controller.Main", {
             onInit: function () {
-                alert("onInit Called");
+			// alert("onInit called!");
+
+			//create a data object
+			var oData = {
+			    "title": "My Orders",
+			 	"SalesOrderSet": [{
+					"SalesOrderID": "0001",
+			 		"CustomerName": "Apple",
+			 		"LifecycleStatusDescription": "Completed",
+			 		"LifecycleStatus": "Success",
+			 		"GrossAmount": "199",
+			 		"CurrencyCode": "USD"
+			 	}, {
+			 		"SalesOrderID": "0002",
+			 		"CustomerName": "Google",
+					"LifecycleStatusDescription": "Completed",
+					"LifecycleStatus": "Success",
+		     		"GrossAmount": "277",
+			 		"CurrencyCode": "USD"
+			 	}, {
+			 		"SalesOrderID": "0003",
+			 		"CustomerName": "Amazon",
+			 		"LifecycleStatusDescription": "Error",
+			 		"LifecycleStatus": "Error",
+			 		"GrossAmount": "599",
+			 		"CurrencyCode": "USD"
+			 	}]
+			 };
+
+			// JSON Model instance
+			 var oModel = new JSONModel(oData);
+			 oModel.setDefaultBindingMode("OneWay");
+
+			// Assign the model to view
+			 this.getView().setModel(oModel);
             },
             onExit: function () {
-                alert("onExit Called");
+            //   alert("onExit Called");
             },
             onBeforeRendering: function () {
-                alert("onBeforeRendering Called");
+            //   alert("onBeforeRendering Called");
             },
             onAfterRendering: function () {
-                alert("onAfterRendering Called");
+            //   alert("onAfterRendering Called");
             },
             onSearch: function () {
                 alert("onSearch Called");
