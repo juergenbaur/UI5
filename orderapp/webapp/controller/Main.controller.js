6449a046fcd6bff2da07769687644b55ee883f88
Juergen Baur <juergen.baur@avisco.de>
2023-08-07 06:44:03 +0000

    OData Call funktioniert

--- orderapp/webapp/controller/Main.controller.js
+++ orderapp/webapp/controller/Main.controller.js
@@ -1,59 +1,62 @@
 sap.ui.define([
     "sap/ui/core/mvc/Controller",
-    "sap/ui/model/json/JSONModel"
+    "sap/ui/model/json/JSONModel",
+    "zju/orderapp/model/formatter"
+
 ],
     /**
      * @param {typeof sap.ui.core.mvc.Controller} Controller
      */
-    function (Controller, JSONModel) {
+    function (Controller, JSONModel, Formatter) {
         "use strict";
-
         return Controller.extend("zju.orderapp.controller.Main", {
+            formatter: Formatter,
             onInit: function () {
-			// alert("onInit called!");
+                // alert("onInit called!");
 
-			//create a data object
-			var oData = {
-			    "title": "My Orders",
-			 	"SalesOrderSet": [{
-					"SalesOrderID": "0001",
-			 		"CustomerName": "Apple",
-			 		"LifecycleStatusDescription": "Completed",
-			 		"LifecycleStatus": "Success",
-			 		"GrossAmount": "199",
-			 		"CurrencyCode": "USD"
-			 	}, {
-			 		"SalesOrderID": "0002",
-			 		"CustomerName": "Google",
-					"LifecycleStatusDescription": "Completed",
-					"LifecycleStatus": "Success",
-		     		"GrossAmount": "277",
-			 		"CurrencyCode": "USD"
-			 	}, {
-			 		"SalesOrderID": "0003",
-			 		"CustomerName": "Amazon",
-			 		"LifecycleStatusDescription": "Error",
-			 		"LifecycleStatus": "Error",
-			 		"GrossAmount": "599",
-			 		"CurrencyCode": "USD"
-			 	}]
-			 };
-
-			// JSON Model instance
-			 var oModel = new JSONModel(oData);
-			 oModel.setDefaultBindingMode("OneWay");
-
-			// Assign the model to view
-			 this.getView().setModel(oModel);
+ /*                              //create a data object
+                                var oData = {
+                                    "title": "My Orders",
+                                    "SalesOrderSet": [{
+                                        "SalesOrderID": "0001",
+                                        "CustomerName": "Apple",
+                                        "LifecycleStatusDescription": "Completed",
+                                        "LifecycleStatus": "Success",
+                                        "GrossAmount": "199",
+                                        "CurrencyCode": "USD"
+                                    }, {
+                                        "SalesOrderID": "0002",
+                                        "CustomerName": "Google",
+                                        "LifecycleStatusDescription": "Completed",
+                                        "LifecycleStatus": "Success",
+                                        "GrossAmount": "277",
+                                        "CurrencyCode": "USD"
+                                    }, {
+                                        "SalesOrderID": "0003",
+                                        "CustomerName": "Amazon",
+                                        "LifecycleStatusDescription": "Error",
+                                        "LifecycleStatus": "Error",
+                                        "GrossAmount": "599",
+                                        "CurrencyCode": "USD"
+                                    }]
+                                };
+                
+                                // JSON Model instance
+                                var oModel = new JSONModel(oData);
+                                oModel.setDefaultBindingMode("OneWay");
+                
+                                // Assign the model to view
+                            this.getView().setModel(oModel);
+*/
             },
             onExit: function () {
-            //   alert("onExit Called");
+                //   alert("onExit Called");
             },
             onBeforeRendering: function () {
-            //   alert("onBeforeRendering Called");
+                //   alert("onBeforeRendering Called");
             },
             onAfterRendering: function () {
-            //   alert("onAfterRendering Called");
+                //   alert("onAfterRendering Called");
             },
             onSearch: function () {
                 alert("onSearch Called");
@@ -64,6 +67,19 @@ sap.ui.define([
             },
             onGroup: function () {
                 alert("onGroup Called");
+            },
+            // formatStatus
+            formatStatus: function (sValue) {
+                switch (sValue) {
+                    case "C":
+                        return ValueState.Success;
+                    case "P":
+                        return ValueState.Warning;
+                    case "X":
+                        return ValueState.Error;
+                    default:
+                        return ValueState.Information;
+                }
             }
         });
     });
